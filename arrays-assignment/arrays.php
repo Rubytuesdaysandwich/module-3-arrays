55ba93374a66fb8c258c5ec99a2a813c64159b66
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-16 15:32:25 -0600

    update to part 2

--- arrays-assignment/arrays.php
+++ arrays-assignment/arrays.php
@@ -11,23 +11,31 @@ part 1:
     <?php echo $line2 = "Then came {$weather[1]} with a few {$weather[2]} and some {$weather[0]}.";//line 2 of phrase?>
     <?php echo $line3 ="At least we didn't get any {$weather[3]} or {$weather[4]}.";//line 3 of phrase?><br/>
     <!-- <?php echo $line1 .$line2 .$line3;// echoing line 1 line2 and line3 three together with concatenation.?> -->
+    <br/>
 part 2:
-    <?php $citys = array("Tokyo" => "Japan", "Mexico City" => "Mexico", "New York City" => "USA", "Mumbai" => "India", "Seoul" => "South Korea" , "Shanghai" =>  "China", "Lagos" => "Nigeria", "Buenos Aires" => "Argentina", "Cairo" => "Egypt", "London" => "England");
+    <?php $citys = array("Japan" => "Tokyo", "Mexico" => "Mexico City" ,"USA" =>  "New York City", "India" => "Mumbai", "South Korea" => "Seoul" , "China" =>"Shanghai"  , "Nigeria" =>"Lagos" , "Argentina" => "Buenos Aires", "Egypt" =>"Cairo" , "England" => "London");
     //associative array contains cities and countries?>
-    <?php echo $citys["Tokyo"] ;?>
-</body>
+    <?php  $keyscountry = (array_keys($citys));//pulling the array keys from the associative array which allow them to be accessed by numbered position below?>
+    <?php  $valuescity = (array_values($citys));//pulling out the values of the array which allows them to be accessed by numbered position below?>
+<br/>
+<?php echo $valuescity[0] . ", " . $keyscountry[0];//these are pulling the items from the array keys and values on line 18 and 19?><br/>
+<?php echo $valuescity[1] . ", " . $keyscountry[1];?><br/>
+<?php echo $valuescity[2] . ", " . $keyscountry[2];?><br/>
+<?php echo $valuescity[3] . ", " . $keyscountry[3];?><br/>
+<?php echo $valuescity[4] . ", " . $keyscountry[4];?><br/>
+<?php echo $valuescity[5] . ", " . $keyscountry[5];?><br/>
+<?php echo $valuescity[6] . ", " . $keyscountry[6];?><br/>
+<?php echo $valuescity[7] . ", " . $keyscountry[7];?><br/>
+<?php echo $valuescity[8] . ", " . $keyscountry[8];?><br/>
+<?php echo $valuescity[9] . ", " . $keyscountry[9];?><br/>
+
+
+
+
+
+
+
+
+
+
 </html>
-<!-- We've seen all kinds of weather this month. At the beginning of the month, we had snow and wind. 
-Then came sunshine with a few clouds and some rain. At least we didn't get any hail or sleet. -->
-
-
-<!-- Tokyo, Japan
-Mexico City, Mexico
-New York City, USA
-Mumbai, India
-Seoul, South Korea
-Shanghai, China
-Lagos, Nigeria
-Buenos Aires, Argentina
-Cairo, Egypt
-London, England. -->
